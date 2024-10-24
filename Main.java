ec1913d241b1b6dd7f31d4c17e12ed12e3de5533
sqvips <rockaturbo@gmail.com>
2024-10-24 18:35:43 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,30 +4,37 @@ public class Main {
 
     public static PrintStream out = System.out;
     public static Scanner in = new Scanner(System.in);
-
     public static void main(String[] args) {
+        
         double a = in.nextDouble();
         double b = in.nextDouble();
-
+        
         double x0 = 0;
-
-        if (a == 0 || b==0){
+        
+        if (a == 0){
             out.println("Решений нет!");
-
         }else{
-
+            
             if (a>0){
-                if (b>0){
-                    out.print("x ∈ ( "+ -b + " ; " + x0 + " )");
-                }else{
+                if (b<0){
+                    
                     out.print("x ∈ ( "+ x0 + " ; " + -b + " )");
+                }else{
+                    if (b>0)
+                        out.print("x ∈ ( "+ -b + " ; " + x0 + " )");
+                    else
+                        out.println("Решений нет!");
                 }
-
             }else{
-                if (b>0){
-                    out.print("x ∈ ( -∞ ; "+ -b + " ) u ( " + x0 + " ; ∞+ )");
-                }else {
+
+                if (b<0){
                     out.print("x ∈ ( -∞ ; " + x0 + " ) u ( " + -b + " ; ∞+ )");
+
+                }else{
+                    if (b>0)
+                        out.print("x ∈ ( -∞ ; "+ -b + " ) u ( " + x0 + " ; ∞+ )");
+                    else
+                        out.print("x ∈ R за исключением {0}");
                 }
             }
         }
